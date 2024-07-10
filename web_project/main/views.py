460e221686edf06cee4e81c7f31ab5ff3f218db5
Дима <dima.tolstikov.2002@gmail.com>
2024-07-10 19:08:42 +0300

    commit4

--- web_project/main/views.py
+++ web_project/main/views.py
@@ -163,7 +163,6 @@ class AddBook(LoginRequiredMixin, DataMixin, CreateView):
     success_url = reverse_lazy('books')
     login_url = reverse_lazy('login')
 
-
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context_from_DataMixin = self.get_user_context(title='Добавить книгу')
